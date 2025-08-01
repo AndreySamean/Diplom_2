66e67f44774ebc8deb0fc5ac3973263ea5f3adbd
AndreyD <andreyndyakonov@gmail.com>
2025-08-01 19:19:05 +0300

    Исправлен класс User.class

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,35 +1,10 @@
 package model;
 
-import com.google.gson.Gson;
 import lombok.Value;
-
-import java.util.Collections;
-import java.util.Map;
 
 @Value
 public class User {
     String email;
     String password;
     String name;
-
-    public String getFieldAsJson(String fieldName) {
-        String value;
-
-        switch (fieldName) {
-            case "email":
-                value = this.email;
-                break;
-            case "password":
-                value = this.password;
-                break;
-            case "name":
-                value = this.name;
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown field: " + fieldName);
-        }
-
-        Map<String, String> fieldMap = Collections.singletonMap(fieldName, value);
-        return new Gson().toJson(fieldMap);
-    }
 }
